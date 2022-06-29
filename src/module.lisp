05179930b4d27381402d24035bdf4b3500f1cf97
Tahara Yoshinori <read.eval.print@gmail.com>
2022-06-29 11:06:31 +0900

    モジュール重なったとき見やすく

--- src/module.lisp
+++ src/module.lisp
@@ -30,3 +30,37 @@
     (setf (.selected-modules *app*) (list self))
     (move-to-front self))
   (call-next-method))
+
+(defmethod render :before ((self module) renderer)
+ (let ((texture (sdl2:create-texture renderer :rgba8888 :target
+                                     (.width self) (.height self))))
+   (sdl2:set-render-target renderer texture)
+   (sdl2:set-texture-blend-mode texture :blend)
+   (sdl2:set-render-draw-color renderer #x00 #x00 #x00
+                               (if (member self (.selected-modules *app*))
+                                   #xbb #x99)) 
+   (sdl2:render-fill-rect
+    renderer
+    (sdl2:make-rect 0 0 (.width self) (.height self)))
+   (sdl2:set-render-target renderer nil)
+   (let ((dest-rect (sdl2:make-rect (.render-x self)
+                                    (.render-y self)
+                                    (.width self)
+                                    (.height self))))
+     (sdl2:render-copy renderer texture :source-rect nil :dest-rect dest-rect))
+   (sdl2:destroy-texture texture)))
+
+(defmethod render :after ((self module) renderer)
+  (let ((color (cond ((member self (.selected-modules *app*))
+                      *selected-module-color*)
+                     ((eq self (.selected-pattern *app*))
+                      *selected-pattern-color*))))
+    (when color
+      (apply #'sdl2:set-render-draw-color renderer color)
+      (sdl2:render-draw-rect
+       renderer
+       (sdl2:make-rect (- (.render-x self) 2)
+                       (- (.render-y self) 2)
+                       (+ (.width self) 4)
+                       (+ (.height self) 4))) )))
+
